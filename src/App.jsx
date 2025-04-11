d95feb407238142566030d5fcac0859ec3b23b08
Keerat Singh <162683689+Keerat7@users.noreply.github.com>
2025-04-11 20:29:23 +0530

    Update App.jsx - dark mode based on hour of the day

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,3 @@
-// // import { useState } from 'react'
-// // import reactLogo from './assets/react.svg'
-// // import viteLogo from '/vite.svg'
-// // import './App.css'
-
-// // function App() {
-// //   const [count, setCount] = useState(0)
-
-// //   return (
-// //     <>
-// //       <div>
-// //         <a href="https://vite.dev" target="_blank">
-// //           <img src={viteLogo} className="logo" alt="Vite logo" />
-// //         </a>
-// //         <a href="https://react.dev" target="_blank">
-// //           <img src={reactLogo} className="logo react" alt="React logo" />
-// //         </a>
-// //       </div>
-// //       <h1>Vite + React</h1>
-// //       <div className="card">
-// //         <button onClick={() => setCount((count) => count + 1)}>
-// //           count is {count}
-// //         </button>
-// //         <p>
-// //           Edit <code>src/App.jsx</code> and save to test HMR
-// //         </p>
-// //       </div>
-// //       <p className="read-the-docs">
-// //         Click on the Vite and React logos to learn more
-// //       </p>
-// //     </>
-// //   )
-// // }
-
-// // export default App
-
 // import React, { useEffect, useState } from "react";
 // import { motion } from "framer-motion";
 // import Hero from "./components/Hero";
@@ -74,13 +38,24 @@
 // }
 
 import { Routes, Route } from 'react-router-dom'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 //import Projects from './pages/Projects'
 //import About from './pages/About'
 
 function App() {
+  useEffect(() => {
+    const hour = new Date().getHours()
+    const prefersDark = hour < 6 || hour >= 18 // 6 PM to 6 AM
+
+    if (prefersDark) {
+      document.documentElement.classList.add('dark')
+    }
+    else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [])
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
       <Navbar />
