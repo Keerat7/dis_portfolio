9d6aff532a46f0a16ebf4b9639ba2585bfe4397a
Keerat Singh <162683689+Keerat7@users.noreply.github.com>
2025-04-14 23:31:15 +0530

    Update Navbar.jsx - Added link for projects page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="p-4 bg-blue-600 text-white">
       <ul className="flex justify-around">
         <li><a href="/">Home</a></li>
-        <li><a href="#">Projects</a></li>
+        <li><a href="/projects">Projects</a></li>
         <li><a href="#">About</a></li>
       </ul>
     </nav>
