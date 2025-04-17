4f6305c9fac740ad7ef0f537ec4426a86c1cab4b
Keerat Singh <162683689+Keerat7@users.noreply.github.com>
2025-04-17 23:10:48 +0530

    Update ProjectCard.jsx - Added qr code and layout adjustments

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,27 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { use, useEffect, useRef, useState } from "react"
 import { motion } from "framer-motion"
 import Tilt from 'react-parallax-tilt'
+import QRCode from "react-qr-code"
 
-export default function ProjectCard({title,description}) {
+export default function ProjectCard({title,description,link}) {
     const cardRef = useRef(null)
     const [constraints, setConstraints] = useState(null)
+    const pointerDown = useRef({ x: 0, y: 0 });
+    const movementThreshold = 10; // pixels
+
+    const handlePointerDown = (e) => {
+        pointerDown.current = { x: e.clientX, y: e.clientY };
+    };
+
+    const handleClick = (e) => {
+        const dx = Math.abs(e.clientX - pointerDown.current.x);
+        const dy = Math.abs(e.clientY - pointerDown.current.y);
+        const moved = dx > movementThreshold || dy > movementThreshold;
+
+        if (!moved) {
+        window.open(link, "_blank", "noopener,noreferrer");
+        }
+    };
 
     useEffect(() => {
         if (cardRef.current) {
@@ -35,14 +52,24 @@ export default function ProjectCard({title,description}) {
                     drag
                     dragConstraints={constraints}
                     dragElastic={0.2}
-                    onDragEnd={(event,info) => {}}
                     animate={{x: 0, y: 0}}
                     transition={{ type: 'spring', stiffness: 300, damping: 20 }}
                     whileHover={{scale:1.05}}
                     whileTap={{scale:0.95}}
+                    onPointerDown={handlePointerDown}
+                    onClick={handleClick}
                 >
-                    <h2 className="text-xl font-semibold mb-2">{title}</h2>
-                    <p className="text-sm text-gray-600 dark:text-gray-300">{description}</p>
+                    <div className="flex items-center gap-6">
+                        <div className="flex-1">
+                            <h2 className="text-xl font-semibold mb-2">{title}</h2>
+                            <p className="text-sm text-gray-600 dark:text-gray-300">{description}</p>
+                        </div>
+                        {link && (
+                            <div className="flex items-center justify-center">
+                                <QRCode value={link} size={100} />
+                            </div>
+                        )}
+                    </div>
                 </motion.div>
             </Tilt>
         </div>
