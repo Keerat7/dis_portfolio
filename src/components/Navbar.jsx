253f665ef6b25358521408f29b7596ba8336369c
Keerat Singh <162683689+Keerat7@users.noreply.github.com>
2025-04-18 21:31:07 +0530

    Update Navbar.jsx - Bug fixes in navbar for the social presence component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav className="p-4 bg-blue-600 text-white">
       <ul className="flex justify-around">
-        <li><a href="/">Home</a></li>
-        <li><a href="/projects">Projects</a></li>
-        <li><a href="#">About</a></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <a href="#">About</a>
+           {/* If 'About' is a separate route, change the above to:
+          <Link to="/about">About</Link>
+          */}
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
