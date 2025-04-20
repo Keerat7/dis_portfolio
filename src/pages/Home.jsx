bc343ae7edb74dfb2d27e9a4a0af1ea7aa202082
Keerat Singh <162683689+Keerat7@users.noreply.github.com>
2025-04-20 17:03:25 +0530

    Update Home.jsx - Updated the layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,23 @@ import ProjectCard from '../components/ProjectCard';
 
 export default function Home() {
   return (
-    <main className="p-8">
+    <main 
+    style={{
+      padding: '2rem',
+      paddingTop: '4rem',
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+    }}>
 
       <motion.h1 
-        className="text-4xl font-bold mb-4"
+        style={{
+          fontSize: '5rem', // equivalent to text-6xl
+          fontWeight: '700',   // font-extrabold
+          marginBottom: '1.5rem',
+          textAlign: 'left',
+        }}
         initial={{ opacity: 0, y: -30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
@@ -16,6 +29,12 @@ export default function Home() {
       </motion.h1>
 
       <motion.p
+        style={{
+          fontSize: '1.5rem', // text-xl
+          textAlign: 'left',
+          maxWidth: '48rem',
+          marginBottom: '4rem'
+        }}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 0.5, duration: 1 }}
@@ -23,16 +42,20 @@ export default function Home() {
         Checkout my design journey and the evolution of my work!
       </motion.p>
 
-      <div className='mt-10 min-h-[300px] overflow-visible relative flex justify-center items-start'>
+      <motion.div className='mt-4 min-h-[300px] overflow-visible relative flex justify-start items-start pr-24'
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1, duration: 0.8 }}
+      >
         <div className='max-w-xs'>
           <h3 className='text-2xl font-bold mb-4 text-center'>Try dragging this 👇</h3>
           <ProjectCard
-            title="a really cool project"
-            description="too cool to be true"
-            link="https://youtube.com"
+            title="Sample Project Card"
+            description="Try clicking on this."
+            link="https://youtu.be/dQw4w9WgXcQ?feature=shared"
           />
         </div>
-      </div>
+      </motion.div>
 
     </main>
   )
